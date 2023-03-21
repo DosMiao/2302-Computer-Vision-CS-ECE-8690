c8f549cd9685e2a96a202641b675f2d0558f4ee4
Xuanbo Miao <31434065+DosMiao@users.noreply.github.com>
2023-03-21 18:58:33 +0000

    main

--- CV2023_HW3B/CVSpring2023_Miao_Xuanbo_Assignment#3B_N3.py
+++ CV2023_HW3B/CVSpring2023_Miao_Xuanbo_Assignment#3B_N3.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     import numpy as np
     import os
 
-    #test git
+    #test git 
     # Check if CUDA is available, else use CPU
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
