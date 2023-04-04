919627b990bcf0b869b20c951ac7d963253c5d1e
Xuanbo Miao <xuanbomiao@gmail.com>
2023-04-03 20:55:21 -0500

    hw4

--- CV2023_HW4A/CVSpring2023_Miao_Xuanbo_Assignment#4A.py
+++ CV2023_HW4A/CVSpring2023_Miao_Xuanbo_Assignment#4A.py
@@ -25,6 +25,26 @@ def segment_image(model, input_tensor):
         output = model(input_tensor)['out'][0]
     return output
 
+def decode_segmap(image, output):
+    _, preds = torch.max(output, 0)
+
+    pascal_voc_colormap = [
+        [0, 0, 0], [128, 0, 0], [0, 128, 0], [128, 128, 0],
+        [0, 0, 128], [128, 0, 128], [0, 128, 128], [128, 128, 128],
+        [64, 0, 0], [192, 0, 0], [64, 128, 0], [192, 128, 0],
+        [64, 0, 128], [192, 0, 128], [64, 128, 128], [192, 128, 128],
+        [0, 64, 0], [128, 64, 0], [0, 192, 0], [128, 192, 0]
+    ]
+    label_colors = np.array(pascal_voc_colormap, dtype=np.uint8)
+
+    rgb = np.zeros((513, 513, 3), dtype=np.uint8)
+    for label in range(0, len(label_colors)):
+        idx = preds == label
+        rgb[idx] = label_colors[label]
+
+    return Image.fromarray(rgb), preds
+
+
 def plot_results(input_image, segmentation_mask, preds):
     pascal_voc_classes = [
         'background', 'aeroplane', 'bicycle', 'bird', 'boat', 'bottle', 'bus', 'car',
@@ -64,19 +84,6 @@ def plot_results(input_image, segmentation_mask, preds):
     return fig
 
 
-
-def plot_results(input_image, segmentation_mask):
-    fig, axes = plt.subplots(1, 2, figsize=(10, 5))
-    axes[0].imshow(input_image)
-    axes[0].set_title('Original Image')
-    axes[1].imshow(segmentation_mask)
-    axes[1].set_title('Segmentation Mask')
-    # plt.show() with 2 seconds pause
-    plt.show(block=False)   # show the image without blocking the code
-    plt.pause(2)            # pause the code execution for 1 second
-    # return the figure
-    return fig
-
 def main():
     test_images = ['./CV2023_HW4A/test_img/HW4a_Test1.jpg', './CV2023_HW4A/test_img/HW4a_Test2.jpg', './CV2023_HW4A/test_img/HW4a_Test3.jpg']
     model = load_model()
@@ -84,11 +91,12 @@ def main():
     for image_path in test_images:
         input_image, input_tensor = preprocess_image(image_path)
         output = segment_image(model, input_tensor)
-        segmentation_mask = decode_segmap(input_image, output)
-        fig = plot_results(input_image, segmentation_mask)
+        segmentation_mask, preds = decode_segmap(input_image, output)
+        fig = plot_results(input_image, segmentation_mask, preds)
         # save the fig with associated image name
         fig.savefig(image_path[:-4] + '_segmentation.png')
         plt.close(fig)
 
 if __name__ == "__main__":
     main()
+
